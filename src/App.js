565f9e22aa0a8fe71f1e83f6b254944f568b4742
VikaOr <ya1oreshek@gmail.com>
2024-06-27 12:59:01 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { 
   BrowserRouter, 
   Routes, 
-  Route } from 'react-router-dom';
+  Route,
+  HashRouter } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
 import Users from './Pages/Users.jsx';
@@ -11,7 +12,7 @@ import MainPage from './Pages/MainPage.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />}>
           <Route index element={<Home /> } />
@@ -21,7 +22,7 @@ function App() {
         </Route>
         
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
